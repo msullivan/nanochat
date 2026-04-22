c84891cd1159f9b040370c177d2d814c84b82f2c
Michael J. Sullivan <sully@msully.net>
2026-04-21 20:33:17 -0700

    setup-runpod: -y on apt, consolidate python3-dev into same call

--- setup-runpod.sh
+++ setup-runpod.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 apt-get update
-apt-get install rsync screen
+apt-get install -y rsync screen python3-dev
 command -v uv &> /dev/null || curl -LsSf https://astral.sh/uv/install.sh | sh
 . ~/.bashrc
 git clone https://github.com/msullivan/nanochat.git
 uvx wandb login
-apt-get install python3-dev
